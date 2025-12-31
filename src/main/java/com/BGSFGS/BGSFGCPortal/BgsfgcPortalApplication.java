7eef4edcbb68cb6bb3a7dbd3895c31c53e988496
pavan a <pavanpavu0018@gmail.com>
2025-12-31 22:53:34 +0530

    project created successfuly

---
+++ src/main/java/com/BGSFGS/BGSFGCPortal/BgsfgcPortalApplication.java
@@ -0,0 +1,13 @@
+package com.BGSFGS.BGSFGCPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgsfgcPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgsfgcPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BGSFGS/BGSFGCPortal/BgsfgcPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BGSFGS.BGSFGCPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgsfgcPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
